4f7383f1b93ef1f041b695fd36acca05be730968
sls_00 <1804048122@qq.com>
2026-03-29 22:29:11 +0800

    md 补充

--- 扁平化实现.js
+++ 扁平化实现.js
@@ -1,1 +1,36 @@
-// 
+
+const tree = [
+  {
+    id: 1,
+    name: 'A',
+    children: [
+      { id: 2, name: 'A-1' },
+      {
+        id: 3,
+        name: 'A-2',
+        children: [{ id: 4, name: 'A-2-1' }]
+      }
+    ]
+  },
+  { id: 5, name: 'B' }
+];
+
+function treeToList(tree, res = []) {
+  for (let node of tree) {
+    const { children, ...item } = node;
+    res.push(item);
+    if (children && children.length) {
+      treeToList(children, res);
+    }
+  }
+  return res;
+}
+
+const map = new Map();
+
+map.delete
+
+console.log(treeToList(tree))
+
+
+// 仅是 -特定结果的树形数组扁平化为清晰结构的数组 
